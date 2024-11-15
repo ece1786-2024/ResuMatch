494f6f0c477de854ee79af8beb1b2e78d5db3bdb
Rafay Kalim <rafay.kalim@mail.utoronto.ca>
2024-11-15 18:58:17 -0500

    clean up datasets API

--- datasets/data.py
+++ datasets/data.py
@@ -1,31 +1,12 @@
 import os
 import pandas as pd
-from datasets.FeatureDataClass import IndustryData, ExperienceLevelData
 
-def get_resume_features_dataset():
-    """
-    Returns pandas dataframe with resume text column and feature columns:
-        1. resume_text: Has resume text
-        2...n feature1, feature2, ..., featuren
+class Dataset:
+    def __init__(self, csv_name) -> None:
+        self.df = pd.read_csv(os.path.join('datasets', 'local_datasets', f'{csv_name}.csv'))
 
-        Current Features:
-            1. Industry: Has industry-like feature (eg: Accountant, Mechanical Engineer, etc)
-            2. ExperienceLevel: One of {Entry, Junior, Mid, Senior, Executive}
-    """
+ResumeFeaturesDataset = Dataset('ResumeFeatures')
+JobResumeMatchesDataset = Dataset('JobResumeMatches')
+LocationValidationDataset = Dataset('Locations')
+ResumeValidationDataset = Dataset('Resumes')
 
-    df = pd.read_csv(os.path.join('datasets', 'local_datasets', 'ResumeFeatures.csv'))
-    
-    return df
-
-def get_resume_job_match_dataset(train_or_test="train"):
-    """
-    Returns pandas dataframe with three columns:
-        1. resume_text: Has resume text
-        2. job_description_text: Has job description text
-        3. label: One of {No Fit, Good Fit, Potential Fit}, whether resume and job description are a fit
-    """
-
-    splits = {'train': 'train.csv', 'test': 'test.csv'}
-    df = pd.read_csv("hf://datasets/cnamuangtoun/resume-job-description-fit/" + splits[train_or_test])
-
-    return df
--- datasets/readme.md
+++ datasets/readme.md
@@ -1,5 +1,6 @@
 # Datasets Used
 
-1. resume <> Industry: This is from Huggingface (https://huggingface.co/datasets/ahmedheakl/resume-atlas).
-2. resume <> Experience Level: This is a dataset made by us. It is stored in `local_datasets/ExperienceLevel.csv`.
-3. resume, job description <> match: This is from HuggingFace (https://huggingface.co/datasets/cnamuangtoun/resume-job-description-fit).
+1. `JobResumeMatches.csv`: This contains a resume, job description, and a label of {Good Fit, No Fit, Potential Fit}. Data source: hf://datasets/cnamuangtoun/resume-job-description-fit/.
+2. `ResumeFeatures.csv`: This contains a resume, Industry, Experience Level. Industry is from Huggingface (https://huggingface.co/datasets/ahmedheakl/resume-atlas). We sampled 150 rows from this dataset. Experience Level is made by us.
+3. `Locations.csv`: This contains a text and a label of {1, 0} where 1 means it is a location, 0 means it is not a location. This is made by us.
+4. `Resumes.csv`: This contains a text and a label of {1, 0} where 1 means it is a resume, 0 means it is not a location. The label-1 data is from `ResumeFeatures.csv`, the label-0 data are IMDB movie reviews.
