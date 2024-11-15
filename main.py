6c2364e7e2a0ccd93978948778e1b5567ae98937
Rafay Kalim <rafay.kalim@mail.utoronto.ca>
2024-11-15 17:59:26 -0500

    run resume parser from main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from datasets import data
+from resume_parser.core import ResumeParser
 
-print(data.get_resume_job_match_dataset())
+api_key = "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"
+model = "gpt-4o"
+
+parser = ResumeParser(api_key=api_key, model=model)
+resume_path = "C:/Users/Rafay/Downloads/rafay_kalim_fin.pdf" # load your resume here
+formatted_data = parser.parse(resume_path)
+print("Extracted features:", formatted_data)
--- resume_parser/main.py
+++ resume_parser/main.py
@@ -4,6 +4,6 @@ api_key = "sk-proj-KEhE_dzl61SKasn3KZh5ZQ7J3HZD-idLY45SP8G1aJTb7SjA1-cYeFye9AnVA
 model = "gpt-4o"
 
 parser = ResumeParser(api_key=api_key, model=model)
-resume_path = "C:/Users/bartc/Desktop/job app/Resume_Cui,W.pdf" # load your resume here
+resume_path = "C:/Users/Rafay/Downloads/rafay_kalim_fin-2.pdf" # load your resume here
 formatted_data = parser.parse(resume_path)
 print("Extracted features:", formatted_data)
