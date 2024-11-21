39f0c557de2ef6ee1b8aedb88299c6c9ed38c027
rafayk7 <rafayk7@yahoo.com>
2024-11-21 11:31:23 -0500

    Revert "Job matching eval"

--- matching_agent/agent_evaluation.py
+++
@@ -1,8 +0,0 @@
-from matching_agent.matching_agent import MatchingAgent
-import pandas as pd
-
-ma = MatchingAgent(name="Job Fit Determination")
-
-df = pd.read_csv('./datasets/local_datasets/JobResumeMatches.csv')
-
-ma.evaluate_dataset(df)
