d5257107ba861ca6aeaf7b8d9c6acc5066f693cc
Karanbir Brar <ks.brar@mail.utoronto.ca>
2024-11-23 13:18:34 -0500

    integrated final

--- location_agent/verify_location.py
+++ location_agent/verify_location.py
@@ -4,12 +4,10 @@ from agent.agent import Agent
 class verify_location(Agent):
     def __init__(self, 
                  name, 
-                #  sys_prompt, 
                  model="gpt-4", 
                  max_tokens=30,
                  response_format={"type": "json_object"},
                  temperature=1.0):
-        
         super().__init__(name, model, max_tokens, response_format, temperature)
         self.json_structure = {
             "industry": "",
